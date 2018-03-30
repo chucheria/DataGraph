a3d2dad301270189ecb2107ed4581cb9ade91905
Bea <bea.hernandez@datmean.com>
2018-03-30 22:52:57 +0200

    Change gulp.js. Delete server module.
    Change starts json with unique titles.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var nodemon = require('gulp-nodemon');
+
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -14,37 +14,18 @@ gulp.task('sass', function() {
         }))
 });
 
-gulp.task('watch', ['sass', 'nodemon'], function() {
-
-    console.log('Starting server')
-    browserSync.init(null, {
-		proxy: "http://localhost:5000",
-        files: ["app/**/*.*"],
-        browser: "google chrome",
-        port: 7000
-	});
-
-
+gulp.task('watch', ['browserSync', 'sass'], function() {
     console.log('Building files');
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 
-
 });
 
-gulp.task('nodemon', function (cb) {
-
-	var started = false;
-
-	return nodemon({
-		script: 'server.js'
-	}).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		if (!started) {
-			cb();
-			started = true;
-		}
-	});
-});
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
